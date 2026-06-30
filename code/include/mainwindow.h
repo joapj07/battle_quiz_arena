4f375d17caa4c92f955fb798ca4d13c4658b637b
joapj07 <joao.pj07@aluno.ifsc.edu.br>
2026-06-29 21:25:33 -0300

    sdfsds

--- code/include/mainwindow.h
+++ code/include/mainwindow.h
@@ -2,48 +2,108 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QWidget>
+#include <QGroupBox>
+#include <QPushButton>
+#include <QLabel>
+#include <QRadioButton>
+#include <QTextBrowser>
+
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QGridLayout>
+
+#include <QVector>
+
 #include "character.h"
 #include "boss.h"
 #include "questionmanager.h"
 #include "item.h"
-#include <QVector>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
-
-class MainWindow : public QMainWindow {
+class MainWindow : public QMainWindow
+{
     Q_OBJECT
 
-private:
-    Ui::MainWindow *ui;
+public:
+    explicit MainWindow(QWidget *parent =nullptr);
+    ~MainWindow();
 
-    // Ponteiros para as instâncias do jogo (POO na prática!)
-    Character* player;
-    Boss* boss;
+private:
+
+    //-----------------------------
+    // COMPONENTES
+    //-----------------------------
+
+    QWidget *centralWidget;
+
+    QGroupBox *groupSelection;
+    QGroupBox *groupCombat;
+    QGroupBox *groupInventory;
+
+    QPushButton *btnSelectWarrior;
+    QPushButton *btnSelectMage;
+    QPushButton *btnSelectArcher;
+
+    QPushButton *btnAnswer;
+    QPushButton *btnUsePotion;
+    QPushButton *btnUseBuff;
+
+    QLabel *lblPlayerHp;
+    QLabel *lblPlayerAtk;
+    QLabel *lblBossHp;
+    QLabel *lblScore;
+    QLabel *lblQuestion;
+
+    QRadioButton *radioOpt1;
+    QRadioButton *radioOpt2;
+    QRadioButton *radioOpt3;
+    QRadioButton *radioOpt4;
+
+    QTextBrowser *txtBattleLog;
+
+    QVBoxLayout *mainLayout;
+    QHBoxLayout *selectionLayout;
+    QGridLayout *combatLayout;
+    QHBoxLayout *inventoryLayout;
+
+    //-----------------------------
+    // LÓGICA
+    //-----------------------------
+
+    Character *player;
+    Boss *boss;
+
     QuestionManager questionManager;
+
     QVector<Item*> inventory;
 
     Question currentQuestion;
+
     int score;
 
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
-
 private slots:
-    // Slots para os botões da interface gráfica
+
     void on_btnSelectWarrior_clicked();
     void on_btnSelectMage_clicked();
     void on_btnSelectArcher_clicked();
+
     void on_btnAnswer_clicked();
+
     void on_btnUsePotion_clicked();
     void on_btnUseBuff_clicked();
 
 private:
+
+    void createInterface();
+
+    void createConnections();
+
     void startMatch();
+
     void nextTurn();
+
     void updateUiStatus();
+
     void checkGameOver();
 };
 
--- code/src/mainwindow.cpp
+++ code/src/mainwindow.cpp
@@ -1,146 +1,400 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
+
 #include "warrior.h"
 #include "mage.h"
 #include "archer.h"
 #include "healthpotion.h"
 #include "attackbuff.h"
+
 #include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent), ui(new Ui::MainWindow), player(nullptr), boss(nullptr), score(0) {
-    ui->setupUi(this);
+    : QMainWindow(parent),
+    player(nullptr),
+    boss(nullptr),
+    score(0)
+{
+    createInterface();
+    createConnections();
 
-    // Esconde os painéis de combate e itens até o jogador escolher um herói
-    ui->groupCombat->setEnabled(false);
-    ui->groupInventory->setEnabled(false);
+    groupCombat->setEnabled(false);
+    groupInventory->setEnabled(false);
 }
 
-MainWindow::~MainWindow() {
-    delete ui;
-    if (player) delete player;
-    if (boss) delete boss;
-    qDeleteAll(inventory); // Limpa a memória dos itens alocados
+MainWindow::~MainWindow()
+{
+    delete player;
+    delete boss;
+    qDeleteAll(inventory);
 }
 
-// --- SELEÇÃO DE PERSONAGENS ---
-void MainWindow::on_btnSelectWarrior_clicked() {
+void MainWindow::createInterface()
+{
+    resize(800,600);
+
+    centralWidget = new QWidget(this);
+    setCentralWidget(centralWidget);
+
+    mainLayout = new QVBoxLayout(centralWidget);
+
+    //------------------------------------------
+    // ESCOLHA DA CLASSE
+    //------------------------------------------
+
+    groupSelection = new QGroupBox("Escolha sua Classe");
+
+    selectionLayout = new QHBoxLayout();
+
+    btnSelectWarrior = new QPushButton("Guerreiro");
+    btnSelectMage = new QPushButton("Mago");
+    btnSelectArcher = new QPushButton("Arqueiro");
+
+    selectionLayout->addWidget(btnSelectWarrior);
+    selectionLayout->addWidget(btnSelectMage);
+    selectionLayout->addWidget(btnSelectArcher);
+
+    groupSelection->setLayout(selectionLayout);
+
+    //------------------------------------------
+    // COMBATE
+    //------------------------------------------
+
+    groupCombat = new QGroupBox("Arena de Batalha");
+
+    combatLayout = new QGridLayout();
+
+    lblPlayerHp = new QLabel("HP: 100/100");
+    lblPlayerAtk = new QLabel("ATK: 15");
+    lblBossHp = new QLabel("HP do Boss: 200/200");
+    lblScore = new QLabel("Pontuação: 0");
+
+    lblQuestion = new QLabel("A pergunta do Quiz aparecerá aqui...");
+    lblQuestion->setWordWrap(true);
+
+    radioOpt1 = new QRadioButton("Opção 1");
+    radioOpt2 = new QRadioButton("Opção 2");
+    radioOpt3 = new QRadioButton("Opção 3");
+    radioOpt4 = new QRadioButton("Opção 4");
+
+    btnAnswer = new QPushButton("Confirmar Resposta ⚔️");
+
+    txtBattleLog = new QTextBrowser();
+
+    combatLayout->addWidget(lblPlayerHp,0,0);
+    combatLayout->addWidget(lblBossHp,1,0);
+    combatLayout->addWidget(lblPlayerAtk,2,0);
+
+    combatLayout->addWidget(lblQuestion,0,1,1,3);
+
+    combatLayout->addWidget(lblScore,0,4);
+
+    combatLayout->addWidget(radioOpt1,1,1);
+    combatLayout->addWidget(radioOpt2,1,2);
+
+    combatLayout->addWidget(radioOpt3,2,1);
+    combatLayout->addWidget(radioOpt4,2,2);
+
+    combatLayout->addWidget(btnAnswer,2,4);
+
+    combatLayout->addWidget(txtBattleLog,3,0,1,5);
+
+    groupCombat->setLayout(combatLayout);
+
+    //------------------------------------------
+    // INVENTÁRIO
+    //------------------------------------------
+
+    groupInventory = new QGroupBox("Mochila");
+
+    inventoryLayout = new QHBoxLayout();
+
+    btnUsePotion = new QPushButton("Usar Poção de Cura");
+    btnUseBuff = new QPushButton("Ativar Especial");
+
+    inventoryLayout->addWidget(btnUsePotion);
+    inventoryLayout->addWidget(btnUseBuff);
+
+    groupInventory->setLayout(inventoryLayout);
+
+    //------------------------------------------
+
+    mainLayout->addWidget(groupSelection);
+    mainLayout->addWidget(groupCombat);
+    mainLayout->addWidget(groupInventory);
+}
+void MainWindow::createConnections()
+{
+    connect(btnSelectWarrior, &QPushButton::clicked,
+            this, &MainWindow::on_btnSelectWarrior_clicked);
+
+    connect(btnSelectMage, &QPushButton::clicked,
+            this, &MainWindow::on_btnSelectMage_clicked);
+
+    connect(btnSelectArcher, &QPushButton::clicked,
+            this, &MainWindow::on_btnSelectArcher_clicked);
+
+    connect(btnAnswer, &QPushButton::clicked,
+            this, &MainWindow::on_btnAnswer_clicked);
+
+    connect(btnUsePotion, &QPushButton::clicked,
+            this, &MainWindow::on_btnUsePotion_clicked);
+
+    connect(btnUseBuff, &QPushButton::clicked,
+            this, &MainWindow::on_btnUseBuff_clicked);
+}
+
+//--------------------------------------------------
+// SELEÇÃO DOS PERSONAGENS
+//--------------------------------------------------
+
+void MainWindow::on_btnSelectWarrior_clicked()
+{
+    if(player)
+        delete player;
+
     player = new Warrior();
+
     startMatch();
 }
 
-void MainWindow::on_btnSelectMage_clicked() {
+void MainWindow::on_btnSelectMage_clicked()
+{
+    if(player)
+        delete player;
+
     player = new Mage();
+
     startMatch();
 }
 
-void MainWindow::on_btnSelectArcher_clicked() {
+void MainWindow::on_btnSelectArcher_clicked()
+{
+    if(player)
+        delete player;
+
     player = new Archer();
+
     startMatch();
 }
 
-// --- CONTROLE DA PARTIDA ---
-void MainWindow::startMatch() {
+//--------------------------------------------------
+// INÍCIO DA PARTIDA
+//--------------------------------------------------
+
+void MainWindow::startMatch()
+{
+    if(boss)
+        delete boss;
+
     boss = new Boss();
+
     score = 0;
+
     questionManager.resetQuestions();
 
-    // Dá itens iniciais ao jogador
     qDeleteAll(inventory);
     inventory.clear();
+
     inventory.append(new HealthPotion());
     inventory.append(new AttackBuff());
 
-    // Desativa a tela de seleção e ativa o combate
-    ui->groupSelection->setEnabled(false);
-    ui->groupCombat->setEnabled(true);
-    ui->groupInventory->setEnabled(true);
+    groupSelection->setEnabled(false);
+    groupCombat->setEnabled(true);
+    groupInventory->setEnabled(true);
+
+    txtBattleLog->clear();
 
     nextTurn();
 }
 
-void MainWindow::nextTurn() {
+//--------------------------------------------------
+// PRÓXIMO TURNO
+//--------------------------------------------------
+
+void MainWindow::nextTurn()
+{
     checkGameOver();
+
     updateUiStatus();
 
-    // Sorteia uma nova pergunta e exibe nos componentes da tela
     currentQuestion = questionManager.getRandomQuestion();
-    ui->lblQuestion->setText(currentQuestion.statement);
 
-    // Atualiza os textos dos RadioButtons com as alternativas
-    ui->radioOpt1->setText(currentQuestion.options[0]);
-    ui->radioOpt2->setText(currentQuestion.options[1]);
-    ui->radioOpt3->setText(currentQuestion.options[2]);
-    ui->radioOpt4->setText(currentQuestion.options[3]);
+    lblQuestion->setText(currentQuestion.statement);
+
+    radioOpt1->setText(currentQuestion.options[0]);
+    radioOpt2->setText(currentQuestion.options[1]);
+    radioOpt3->setText(currentQuestion.options[2]);
+    radioOpt4->setText(currentQuestion.options[3]);
+
+    radioOpt1->setChecked(false);
+    radioOpt2->setChecked(false);
+    radioOpt3->setChecked(false);
+    radioOpt4->setChecked(false);
 }
+//--------------------------------------------------
+// RESPOSTA DO JOGADOR
+//--------------------------------------------------
 
-void MainWindow::on_btnAnswer_clicked() {
+void MainWindow::on_btnAnswer_clicked()
+{
     int selectedIndex = -1;
-    if (ui->radioOpt1->isChecked()) selectedIndex = 0;
-    else if (ui->radioOpt2->isChecked()) selectedIndex = 1;
-    else if (ui->radioOpt3->isChecked()) selectedIndex = 2;
-    else if (ui->radioOpt4->isChecked()) selectedIndex = 3;
 
-    if (selectedIndex == -1) {
-        QMessageBox::warning(this, "Aviso", "Selecione uma alternativa antes de responder!");
+    if (radioOpt1->isChecked())
+        selectedIndex = 0;
+    else if (radioOpt2->isChecked())
+        selectedIndex = 1;
+    else if (radioOpt3->isChecked())
+        selectedIndex = 2;
+    else if (radioOpt4->isChecked())
+        selectedIndex = 3;
+
+    if (selectedIndex == -1)
+    {
+        QMessageBox::warning(this,
+                             "Aviso",
+                             "Selecione uma alternativa antes de responder!");
         return;
     }
 
-    QString logCombate = "";
+    QString logCombate;
 
-    // Validação da Resposta
-    if (selectedIndex == currentQuestion.correctOptionIndex) {
+    if (selectedIndex == currentQuestion.correctOptionIndex)
+    {
         score += 10;
-        logCombate = "✅ Resposta Correta!\n" + player->attack(boss);
-    } else {
-        logCombate = "❌ Resposta Errada!\n" + boss->getRandomInsult() + "\n" + boss->attack(player);
+
+        logCombate =
+            "✅ Resposta Correta!\n" +
+            player->attack(boss);
+    }
+    else
+    {
+        logCombate =
+            "❌ Resposta Errada!\n" +
+            boss->getRandomInsult() +
+            "\n" +
+            boss->attack(player);
     }
 
-    ui->txtBattleLog->append(logCombate);
+    txtBattleLog->append(logCombate);
+
     nextTurn();
 }
 
-// --- USO DE ITENS (POLIMORFISMO) ---
-void MainWindow::on_btnUsePotion_clicked() {
-    for (int i = 0; i < inventory.size(); ++i) {
-        if (dynamic_cast<HealthPotion*>(inventory[i])) {
+//--------------------------------------------------
+// POÇÃO
+//--------------------------------------------------
+
+void MainWindow::on_btnUsePotion_clicked()
+{
+    for (int i = 0; i < inventory.size(); i++)
+    {
+        if (dynamic_cast<HealthPotion*>(inventory[i]))
+        {
             inventory[i]->applyEffect(player);
-            ui->txtBattleLog->append("🧪 Você usou uma Poção de Cura! +50 HP.");
+
+            txtBattleLog->append(
+                "🧪 Você usou uma Poção de Cura! +50 HP."
+                );
+
+            delete inventory[i];
             inventory.removeAt(i);
+
             break;
         }
     }
+
     updateUiStatus();
 }
 
-void MainWindow::on_btnUseBuff_clicked() {
-    for (int i = 0; i < inventory.size(); ++i) {
-        if (dynamic_cast<AttackBuff*>(inventory[i])) {
+//--------------------------------------------------
+// BUFF
+//--------------------------------------------------
+
+void MainWindow::on_btnUseBuff_clicked()
+{
+    for (int i = 0; i < inventory.size(); i++)
+    {
+        if (dynamic_cast<AttackBuff*>(inventory[i]))
+        {
             inventory[i]->applyEffect(player);
-            ui->txtBattleLog->append("⚡ Overclock Ativado! Seu ataque aumentou permanentemente.");
+
+            txtBattleLog->append(
+                "⚡ Overclock Ativado! Seu ataque aumentou permanentemente."
+                );
+
+            delete inventory[i];
             inventory.removeAt(i);
+
             break;
         }
     }
+
     updateUiStatus();
 }
 
-// --- ATUALIZAÇÃO DO VISUAL E REGRAS ---
-void MainWindow::updateUiStatus() {
-    if (!player || !boss) return;
+//--------------------------------------------------
+// ATUALIZA A INTERFACE
+//--------------------------------------------------
 
-    ui->lblPlayerHp->setText("HP: " + QString::number(player->getHp()) + " / " + QString::number(player->getMaxHp()));
-    ui->lblPlayerAtk->setText("ATK: " + QString::number(player->getCurrentAttackPower()));
-    ui->lblBossHp->setText("HP do Boss: " + QString::number(boss->getHp()) + " / " + QString::number(boss->getMaxHp()));
-    ui->lblScore->setText("Pontuação: " + QString::number(score));
+void MainWindow::updateUiStatus()
+{
+    if (!player || !boss)
+        return;
+
+    lblPlayerHp->setText(
+        "HP: " +
+        QString::number(player->getHp()) +
+        " / " +
+        QString::number(player->getMaxHp())
+        );
+
+    lblPlayerAtk->setText(
+        "ATK: " +
+        QString::number(player->getCurrentAttackPower())
+        );
+
+    lblBossHp->setText(
+        "HP do Boss: " +
+        QString::number(boss->getHp()) +
+        " / " +
+        QString::number(boss->getMaxHp())
+        );
+
+    lblScore->setText(
+        "Pontuação: " +
+        QString::number(score)
+        );
 }
 
-void MainWindow::checkGameOver() {
-    if (boss->getHp() <= 0) {
-        QMessageBox::information(this, "Vitória!", "Parabéns! Você derrotou o Grão-Mestre do Silício e compilou seu código com sucesso!");
-        this->close();
-    } else if (player->getHp() <= 0) {
-        QMessageBox::critical(this, "Game Over", "Seu HP chegou a zero... O Grão-Mestre fritou seus barramentos. Código com Kernel Panic!");
-        this->close();
+//--------------------------------------------------
+// FIM DE JOGO
+//--------------------------------------------------
+
+void MainWindow::checkGameOver()
+{
+    if (!player || !boss)
+        return;
+
+    if (boss->getHp() <= 0)
+    {
+        QMessageBox::information(
+            this,
+            "Vitória!",
+            "Parabéns! Você derrotou o Grão-Mestre do Silício e compilou seu código com sucesso!"
+            );
+
+        close();
+        return;
+    }
+
+    if (player->getHp() <= 0)
+    {
+        QMessageBox::critical(
+            this,
+            "Game Over",
+            "Seu HP chegou a zero... O Grão-Mestre fritou seus barramentos. Código com Kernel Panic!"
+            );
+
+        close();
     }
 }
